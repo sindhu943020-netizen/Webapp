c5349944377f49acc60ba83dac91b0213ff4718c
sindhu943020-netizen <sindhu943020@gmail.com>
2026-04-28 17:52:10 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const cacheName = 'medshield-cache-v1';
+const assets = ['./', './index.html', './manifest.json', './logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
